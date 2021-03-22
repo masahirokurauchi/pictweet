05525045bc69f3d3bd868fdd160c55da62ad3200
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-22 16:01:58 +0900

    good test

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,13 +8,19 @@ RSpec.describe Tweet, type: :model do
   describe 'ツイートの保存' do
     context 'ツイートが投稿できる場合' do
       it '画像とテキストがあれば投稿できる' do
+        expect(@tweet).to be_valid
       end
+
       it 'テキストがあれば投稿できる' do
+        @tweet.image = ''
+        expect(@tweet).to be_valid
       end
     end
+
     context 'ツイートが投稿できない場合' do
       it 'テキストが空では投稿できない' do
       end     
+
       it 'ユーザーが紐付いていなければ投稿できない' do
       end
     end
