7f7145c34fcc538374f4b85e95ac7c0683846336
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-22 16:04:47 +0900

    no user test

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -25,6 +25,9 @@ RSpec.describe Tweet, type: :model do
       end     
 
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @tweet.user = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('User must exist')
       end
     end
   end
