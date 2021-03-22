27426dddf81c84dbd07ef89d75368a017842520f
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-22 15:06:59 +0900

    password blank test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,9 +33,13 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
-    
+
     it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+    
     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
     end
     it 'nicknameが7文字以上では登録できない' do
