ddb902e584f98e7bf783eb0baf09107f85b8f7c7
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-23 15:34:59 +0900

    get root_path

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -7,11 +7,16 @@ describe TweetsController, type: :request do
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get root_path
+      binding.pry
     end
+
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
     end
+
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
     end
+
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
     end
   end
